bea241b58c1a65e8915239cfe4c4f81a949dd850
Marlon <>
2023-10-31 19:10:11 +0800

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import App from "./App";
+
+import ReactDom from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import AuthProvider from "./context/AuthContext";
+import { QueryProvider } from "./lib/react-query/QueryProvider";
+
+ReactDom.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <QueryProvider>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </QueryProvider>
+  </BrowserRouter>
+);
